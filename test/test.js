c7ad9cffe8e66810e9d515afa03d185f92982c23
John Medland <jcmedland@gmail.com>
2017-05-17 17:14:42 +0100

    More testing, less console output on test fn fail.

--- test/test.js
+++ test/test.js
@@ -4,51 +4,77 @@ let path = require('path')
 
 const PLAY = `let play=a=>{//generated at ${new Date()}
 return a}`
+const BADPLAY = `let play=a=>{}/*EXTRA }*/}`
 const TESTPASS = `let test = () => true`
 const TESTFAIL = `let test = () => false`
 
-const TEST_DIR = path.join(__dirname,'.TEST')
-const INPUT_FILE = path.join(TEST_DIR,'t.js')
-const OUTPUT_FILE = path.join(TEST_DIR,'t.caddy.js')
+const TEST_DIR = path.join(__dirname, '.TEST')
+const INPUT_FILE = path.join(TEST_DIR, 't.js')
+const OUTPUT_FILE = path.join(TEST_DIR, 't.caddy.js')
 
 // a promise based sleep
-let sleep = time => {
+let sleep = (time = 2000) => {
   return new Promise(resolve => {
     setTimeout(resolve, time)
   })
 }
 
-let caddy = null;
-
 // Empty testing dir
 fs.emptyDir(TEST_DIR)
   // write testing file
-  .then(()=> fs.writeFile(INPUT_FILE, PLAY + '\n' + TESTPASS))
+  .then(() => fs.writeFile(INPUT_FILE, PLAY + '\n' + TESTPASS))
   // start caddy and sleep
-  .then( () =>{
-    caddy = fork(path.join(__dirname, '../bin/caddy'), [INPUT_FILE])
-    return sleep(2000)
+  .then(() => {
+    console.log('** WAITING FOR A FORK TO LAUNCH **')
+    fork(path.join(__dirname, '../bin/caddy'), [INPUT_FILE])
+    console.log('** TESTING THAT A FILE IS GENERATED FROM SUITABLE SCRIPT **')
+    return sleep(3000)
   })
-  //read output file
-  .then(()=> fs.readFile(OUTPUT_FILE,'utf-8'))
+  // read output file
+  .then(() => fs.readFile(OUTPUT_FILE, 'utf-8'))
   // compare expected
-  .then(output =>{
+  .then(output => {
     if (output !== PLAY) {
-      throw new Error('Output file does not match input file')
+      throw new Error('Output file should match the input file')
     }
+    console.log('** TESTING THAT TEST FUNCTION WORKS **')
     return fs.writeFile(INPUT_FILE, PLAY + '\n' + TESTFAIL)
   })
   // wait for caddy
-  .then(() => sleep(2000))
+  .then(sleep)
   // read file again
-  .then(()=> fs.readFile(OUTPUT_FILE,'utf-8'))
+  .then(() => fs.readFile(OUTPUT_FILE, 'utf-8'))
   // compare expected
-  .then(output =>{
+  .then(output => {
     if (output !== PLAY) {
-      throw new Error('Output file does not match input file')
+      throw new Error('Test function should have lead to test fail')
     }
-    //all done
-    console.log('**TESTING COMPLETE - PASS**')
+    console.log('** TESTING THAT A MISSING PLAY FUNCITON LEADS TO AN ERROR **')
+    return fs.writeFile(INPUT_FILE, TESTPASS)
+  })
+  // wait for caddy
+  .then(sleep)
+  // read file again
+  .then(() => fs.readFile(OUTPUT_FILE, 'utf-8'))
+  // compare expected
+  .then(output => {
+    if (output !== PLAY) {
+      throw new Error('Play function should not have been found.')
+    }
+    console.log('** TESTING THAT A BAD PLAY FUNCTION LEADS TO AN ERROR **')
+    return fs.writeFile(INPUT_FILE, BADPLAY)
+  })
+  // wait for caddy
+  .then(sleep)
+  // read file again
+  .then(() => fs.readFile(OUTPUT_FILE, 'utf-8'))
+  // compare expected
+  .then(output => {
+    if (output !== PLAY) {
+      throw new Error('Script should not have parsed interpreter')
+    }
+    // all done
+    console.log('** TESTING COMPLETE - PASS **')
     process.exit(0)
   })
   .catch(err => {
